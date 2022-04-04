afea3429f0ae5ed6c31601787a681b83e1233a3f
kedaroo <krb1202@gmail.com>
2022-04-04 23:42:12 +0530

    style: dropshadow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
 
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
 
 
@@ -12,10 +14,17 @@ export default function Navbar() {
     <nav>
       <h1>my reading bucket list</h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li onClick={logout}>Logout</li>
+        {user ? 
+          <>
+            <li><Link to="/">Home</Link></li>
+            <li onClick={logout}>Logout</li> 
+          </>
+          :
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        }
       </ul>
     </nav>
   )
